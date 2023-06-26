6da6bac46347ee4f230206b6896fbd42003de334
Angelo <angeloghiotto@gmail.com>
2023-06-26 15:21:21 -0300

    Creating event

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -12,7 +12,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        return \App\Models\Event::all();
     }
 
     /**
@@ -20,15 +20,22 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $event = \App\Models\Event::create([...$request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'required|date',
+            'end_time' => 'required|date|after:start_time',
+        ]), 'user_id' => 1]);
+
+        return $event;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(\App\Models\Event $event)
     {
-        //
+        return $event;
     }
 
     /**
