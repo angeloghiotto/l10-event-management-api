53e713dba2f9455c45ba5efcb4780fd2a0921cc4
Angelo <angeloghiotto@gmail.com>
2023-06-27 16:42:45 -0300

    Event scheduler

---
+++ app/Console/Commands/SendEventReminders.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Event;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class SendEventReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-event-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send notifications to all event attendees that event start next day.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $events = Event::with('attendees.user')->whereBetween('start_time', [
+            now(),
+            now()->addDay(),
+        ])->get();
+
+        $eventsCount = $events->count();
+        $eventLabel = Str::plural('event', $eventsCount);
+
+        $this->info("Found {$eventsCount} {$eventLabel} to send notifications for.");
+
+        $events->each(fn ($event) => $event->attendees->each(fn ($attendee) => $this->info("Sending notification to {$attendee->user->name} for {$event->name}")));
+
+        $this->info('Notifications sent!');
+    }
+}
